d32bf66002b31dc251df587a708661407a54ce6b
lmannier <lmannier1337@gmail.com>
2025-01-03 00:25:21 +0100

    add d32, the automated birthday wisher email

---
+++ d32-birthday_wisher/main.py
@@ -0,0 +1,35 @@
+import smtplib
+from random import randint, choice
+import pandas as pd
+from datetime import datetime
+
+EMAIl = "baltarzeasy@gmail.com"
+PASSWORD = "nhnh zttc hxeq zbnd"
+
+date = datetime.today()
+actual_date = (date.month, date.day)
+
+data = pd.read_csv('birthdays.csv')
+birthdays_dict = {(row.month, row.day): row for index, row in data.iterrows()}
+birthdays_date = birthdays_dict.keys()
+for birthday in birthdays_date:
+    if actual_date == birthday:
+        letters = choice(["letter_1.txt", "letter_2.txt", "letter_3.txt"])
+
+        try:
+            with open(f"letter_templates/{letters}", "r") as f:
+                letter = f.read()
+        except FileNotFoundError:
+            print("letter file not found")
+        else:
+            letter = letter.replace("[NAME]", birthdays_dict[birthday]["name"])
+        finally:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(EMAIl, PASSWORD)
+                smtp.sendmail(
+                    to_addrs=birthdays_dict[birthday]["email"],
+                    from_addr=EMAIl,
+                    msg=f"Subject: Birthday wisher!\n\n{letter}")
+
+
+
